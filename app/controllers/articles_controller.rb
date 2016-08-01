12e87d92231aa0cd09e5816a35ce8487a44cd013
Melanie Hahn <melaniesarah@gmail.com>
2016-07-31 17:30:51 -0800

    Follow along blog creating controllers views and models

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+
+    @article.save
+    redirect_to @article
+  end
+end
